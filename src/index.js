9fdad17b86182b9475fd904dd41e260ab73b5a4a
Mitch <MitchelClark1997@outlook.com>
2021-08-03 08:59:02 +1000

    adding buttons and button css

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import Events from './event-listeners'
 import './style.scss'
 
 const user = User();
-const example = Project('Daily Fitness', 'My reoccurring goal to get in a healthy amount of body movement and exertion.', 1)
+const example = Project('Example Project', 'My reoccurring goal to get in a healthy amount of body movement and exertion.', 1)
 
 user.newProject(example);
-user.printProject()
+user.printProject();
+user.domPrinter();
 
 
 ProjectFormEvents();
--- src/user.js
+++ src/user.js
@@ -17,6 +17,8 @@ export default function() {
     const proSec = document.createElement('section');
     const proSecTit = document.createElement('section');
     const proTit = document.createElement('h2');
+    const proDesEdiBtn = document.createElement('button')
+    const proDesHidBtn = document.createElement('button')
     const proDesCont = document.createElement('div');
     const proDes = document.createElement('p');
     const todSec = document.createElement('section');
@@ -25,6 +27,8 @@ export default function() {
     proSec.classList.add('project');
     proSecTit.classList.add('project-title-area');
     proTit.classList.add('project-title');
+    proDesEdiBtn.classList.add('edit-button', 'sec-btns');
+    proDesHidBtn.classList.add('hide-button', 'sec-btns')
     proDesCont.classList.add('project-des');
     todSec.classList.add('project-todos');
     todList.classList.add('todos');
@@ -32,13 +36,19 @@ export default function() {
     main.appendChild(proSec);
     proSec.appendChild(proSecTit);
     proSecTit.appendChild(proTit);
+    proSecTit.appendChild(proDesEdiBtn);
+    proSecTit.appendChild(proDesHidBtn);
     proSecTit.appendChild(proDesCont);
     proDesCont.appendChild(proDes);
     proSec.appendChild(todSec);
     todSec.appendChild(todList);
 
-    proDes.innerHTML = description;
     proTit.innerHTML = title;
+    proTit.contentEditable = 'true';
+    proDes.innerHTML = description;
+    proDes.contentEditable = 'true';
+    proDesEdiBtn.innerHTML = '&#9998;';
+    proDesHidBtn.innerHTML = '+';
   }
 
   const domPrinter = () => {
